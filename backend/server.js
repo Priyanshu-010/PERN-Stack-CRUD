7425a5ca14861e1c08c520e43686b57dbbc3b2da
Priyanshu-010 <priyanshurai2772@gmail.com>
2025-08-10 21:49:34 +0530

    Basic Backend Setup

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(cors());
+app.use(helmet());
+app.use(morgan('dev'));
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(PORT,()=>{
+  console.log('Listening on port ' + PORT);
+})
