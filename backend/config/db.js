c24fecff720d23410776bcc56e7cf99dc4f27d73
Priyanshu-010 <priyanshurai2772@gmail.com>
2025-08-11 16:22:59 +0530

    Database setup

---
+++ backend/config/db.js
@@ -0,0 +1,11 @@
+import { neon } from "@neondatabase/serverless";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const {PGHOST, PGDATABASE, PGUSER, PGPASSWORD} = process.env;
+
+//This creates a SQL connection to the Neon database using the environment variables.
+export const sql = neon(
+  `postgresql://${PGUSER}:${PGPASSWORD}@${PGHOST}/${PGDATABASE}?sslmode=require&channel_binding=require`
+)
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,11 @@
-import express from 'express';
-import helmet from 'helmet';
-import morgan from 'morgan';
-import cors from 'cors';
-import dotenv from 'dotenv';
+import express from "express";
+import helmet from "helmet";
+import morgan from "morgan";
+import cors from "cors";
+import dotenv from "dotenv";
 
-import productRoutes from './routes/productRoutes.js'
+import productRoutes from "./routes/productRoutes.js";
+import { sql } from "./config/db.js";
 
 dotenv.config();
 const app = express();
@@ -13,10 +14,29 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(cors());
 app.use(helmet());
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
-app.use('/api/products', productRoutes)
+app.use("/api/products", productRoutes);
 
-app.listen(PORT,()=>{
-  console.log('Listening on port ' + PORT);
-})
+async function initDb() {
+  try {
+    await sql`
+      CREATE TABLE IF NOT EXISTS products(
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(255) NOT NULL,
+        image VARCHAR(255) NOT NULL,
+        price DECIMAL(10, 2) NOT NULL,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+      )
+    `;
+    console.log("Database initialized successfully");
+  } catch (error) {
+    console.log("Error in initDB ", error);
+  }
+}
+
+initDb().then(() => {
+  app.listen(PORT, () => {
+    console.log("Listening on port " + PORT);
+  });
+});
